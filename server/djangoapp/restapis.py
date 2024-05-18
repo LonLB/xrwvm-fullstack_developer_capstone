fc12ee354b44313be64a2419b761207b153c4300
LonLB <banasihan.marlon0809@gmail.com>
2024-05-18 13:47:46 -0400

    Week 3 Commits

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,12 +12,13 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 
 def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
     params = ""
     if(kwargs):
         for key,value in kwargs.items():
             params=params+key+"="+value+"&"
+
     request_url = backend_url+endpoint+"?"+params
+
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
@@ -29,20 +30,18 @@ def get_request(endpoint, **kwargs):
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
     try:
-         # Call get method of requests library with URL and parameters
-        response = response.get(request_url)
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
         return response.json()
-     except Exception as err:
+    except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
 def post_review(data_dict):
-# Add code for posting review
-    request_url = backend_url + "/insert_review"
+    request_url = backend_url+"/insert_review"
     try:
-        response = request.post(request_url, json=data_dict)
+        response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
     except:
