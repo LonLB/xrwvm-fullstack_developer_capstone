e1baa9faec0b9de1eb3807b8dc8d504cfdbc10cb
LonLB <165409401+LonLB@users.noreply.github.com>
2024-05-19 21:53:39 +0800

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -100,9 +100,9 @@ def get_cars(request):
 def get_dealerships(request, state="All"):
     if state == "All":
         endpoint = "/fetchDealers"
-    else Exception:
+    else:
         endpoint = "/fetchDealers/" + state
-    dealerships = get_request(endpoint)
+        dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 # ...
 
@@ -113,7 +113,7 @@ def get_dealer_details(request, dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status":200,"dealer":dealership})
-    else Exception:
+    else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 # ...
 
